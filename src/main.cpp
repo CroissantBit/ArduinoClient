84f2a203e051b9ac6bbd5afc7e080b3364fd7d3a
frkri <57220519+frkri@users.noreply.github.com>
2023-10-22 21:26:19 +0200

    Update simple example

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,42 @@
 #include <Arduino.h>
+#include <pb_arduino.h>
 #include <pb_encode.h>
 #include <pb_decode.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Protobuf generated file
+#include "common.pb.c"
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+bool prefix(pb_ostream_t *stream, int msgid)
+{
+  return pb_write(stream, (uint8_t *)&msgid, sizeof(msgid));
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup()
+{
+  Serial.begin(115200);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  SimpleMessage message = SimpleMessage_init_zero;
+
+  pb_ostream_t stream = as_pb_ostream(Serial);
+
+  /* Fill in the lucky number */
+  message.lucky_number = 13;
+  strcpy(message.message, "Hello world!");
+
+  /* Now we are ready to encode the message! */
+  prefix(&stream, 1);
+  status = pb_encode(&stream, &SimpleMessage_msg, &message);
+  message_length = stream.bytes_written;
+
+  /* Then just check for any errors.. */
+  if (!status)
+  {
+    Serial.print("Encoding failed: ");
+    Serial.println(PB_GET_ERROR(&stream));
+    return;
+  }
+  delay(2000);
 }
